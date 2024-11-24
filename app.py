e660125c73ed4995dc095f91a9f27c78f37aad67
Javi D <160622695+JaviDoria@users.noreply.github.com>
2024-11-24 21:22:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -91,16 +91,9 @@ for col in required_columns:
         user_encoded_data[col] = 0
 
 # Asegurarse de que las columnas estén en el orden correcto
-user_encoded_data = user_encoded_data[required_columns]
+# user_encoded_data = user_encoded_data[required_columns]
 
-''' # Verificar que las columnas 'age' y 'balance' estén presentes en el DataFrame antes de la estandarización
-if 'age' not in user_encoded_data.columns or 'balance' not in user_encoded_data.columns:
-    st.error("Las columnas 'age' y 'balance' deben estar presentes para la estandarización.")
-else:
-    # Estandarizar las entradas de edad y saldo usando el escalador cargado
-    scale_variable = ['age', 'balance']
-    user_encoded_data[scale_variable] = scaler.transform(user_encoded_data[scale_variable])
-'''
+
 # Realizar la predicción
 prediction = model.predict(required_columns)
 
